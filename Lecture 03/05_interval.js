90abdfeb3e995a67b16727a6e84845d8d54254e9
Tekane Chaitanya <71593995+chaitanyatekane@users.noreply.github.com>
2021-06-27 21:11:48 +0530

    Added File 05

---
+++ Lecture 03/05_interval.js
@@ -0,0 +1,22 @@
+function happy(seconds){
+    console.log("I am happy after " + seconds + " Seconds");
+}
+
+// let another = function(seconds){
+//     console.log("I am happy after " + seconds + " Seconds");
+// }
+
+// another(6);
+
+
+// console.log(another);
+
+
+
+// for(let i=0; i<10; i++){
+
+// }
+
+setTimeout(function(){
+    happy(2);
+}, 2000);
